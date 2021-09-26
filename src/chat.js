27bd04c518911dfa542f44b0618decaa0667b769
Mariana <marianaufc@gmail.com>
2021-09-26 10:18:31 -0300

    Adding chats to the database

--- src/chat.js
+++ src/chat.js
@@ -1,2 +1,31 @@
 import db from './firestore.js';
-console.log(db);
+import { collection, doc, setDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
+
+const chats = collection(db, "chats");
+
+class Chatroom {
+    constructor(room, username) {
+        this.room = room;
+        this.username = username;
+        this.chats = chats;
+    }
+    async addChat(message) {
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            message: message,
+            username: this.username,
+            room: this.room,
+            created_at: Timestamp.fromDate(now)
+        }
+        const chatRef = doc(collection(db, "chats"));
+        const response = await setDoc(chatRef, chat);
+        return response;
+    }
+}
+
+const chatroom = new Chatroom('gaming', 'shaun');
+chatroom.addChat("hello everyone")
+    .then(() => console.log("chat added"))
+    .catch((err) => console.log(err));
+console.log(chatroom);
--- src/firestore.js
+++ src/firestore.js
@@ -1,6 +1,7 @@
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
     import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
+    
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,5 @@
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
+
     export default db;
