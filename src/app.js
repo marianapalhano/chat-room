20b8f779a9c595d44efea88e2e25dfdf3b452f75
Mariana <marianaufc@gmail.com>
2021-10-08 11:17:59 -0300

    Taking messages from form and adding to the chatroom

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ import { ChatUI } from "./ui.js";
 
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add a new chat
+newChatForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+})
 
 // class instances 
 const chatUI = new ChatUI(chatList);
