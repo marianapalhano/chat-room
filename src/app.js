cb2788b2ca40c8d0daac6b81a4ace321aa16a208
Mariana <marianaufc@gmail.com>
2021-10-09 09:28:47 -0300

    feat: Update name and send message

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { ChatUI } from "./ui.js";
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg');
 
 // add a new chat
 newChatForm.addEventListener('submit', evt => {
@@ -12,6 +14,16 @@ newChatForm.addEventListener('submit', evt => {
     chatroom.addChat(message)
         .then(() => newChatForm.reset())
         .catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMsg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMsg.innerText = '', 3000);
 })
 
 // class instances 
